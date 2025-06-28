398ad428b0483c511da5ec91f5ef677ebe01c8ab
C0dName <tortum1996@gmail.com>
2025-06-28 14:47:22 +0300

    Доработал по гайду элемент добавления товара

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavButton from "./Components/UI/Button/NavButton";
 // import Products from "./Components/UI/ProductBlock/Products";
 import ProductConteiner from "./Components/UI/ProductBlock/ProductConteiner";
 import Input1 from "./Components/UI/Input/Input1";
+import AddProduct from "./Components/AddProduct";
 // import AddNewProduct from "./Components/AddNewProduct";
 // import AddProduct from "./Components/AddProduct";
 
@@ -16,23 +17,27 @@ function App() {
         // {id:2, name: 'название', price: 'Цена', rating: 'рейтинг'},
     ]);
 
-    const [title, setTitle] = useState('');
-    const [title1, setTitle1] = useState('');
-    const [title2, setTitle2] = useState('');
+    // const [title, setTitle] = useState('');
+    // const [title1, setTitle1] = useState('');
+    // const [title2, setTitle2] = useState('');
 
-    const addNewProduct = (e) => {
-        e.preventDefault();
-        const newProduct = {
-            id: Date.now(),
-            title,
-            title1,
-            title2
-        }
+    // const addNewProduct = (e) => {
+    //     e.preventDefault();
+    //     const newProduct = {
+    //         id: Date.now(),
+    //         title,
+    //         title1,
+    //         title2
+    //     }
+    //     setPosts([...posts, newProduct]);
+    //     setTitle('')
+    //     setTitle1('')
+    //     setTitle2('')
+    // };
+
+    const createProduct = (newProduct) => {
         setPosts([...posts, newProduct]);
-        setTitle('')
-        setTitle1('')
-        setTitle2('')
-    };
+    }
 
     return (
         <div>
@@ -42,29 +47,30 @@ function App() {
                 Каталог пердежей
             </NavButton>
             <ProductConteiner posts={posts}/>
-            <div>
-            <form>
-                <Input1
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                    type="text"
-                    placeholder="Название товара"
-                />
-                <Input1
-                    value={title1}
-                    onChange={e => setTitle1(e.target.value)}
-                    type="number"
-                    placeholder="Цена"
-                />
-                <Input1
-                    value={title2}
-                    onChange={e => setTitle2(e.target.value)}
-                    type="text"
-                    placeholder="Рейтинг"
-                />
-                <NavButton onClick={addNewProduct}>Отправить</NavButton>
-            </form>
-            </div>
+            <AddProduct create={createProduct}/>
+            {/*<div>*/}
+            {/*<form>*/}
+            {/*    <Input1*/}
+            {/*        value={title}*/}
+            {/*        onChange={e => setTitle(e.target.value)}*/}
+            {/*        type="text"*/}
+            {/*        placeholder="Название товара"*/}
+            {/*    />*/}
+            {/*    <Input1*/}
+            {/*        value={title1}*/}
+            {/*        onChange={e => setTitle1(e.target.value)}*/}
+            {/*        type="number"*/}
+            {/*        placeholder="Цена"*/}
+            {/*    />*/}
+            {/*    <Input1*/}
+            {/*        value={title2}*/}
+            {/*        onChange={e => setTitle2(e.target.value)}*/}
+            {/*        type="text"*/}
+            {/*        placeholder="Рейтинг"*/}
+            {/*    />*/}
+            {/*    <NavButton onClick={addNewProduct}>Отправить</NavButton>*/}
+            {/*</form>*/}
+            {/*</div>*/}
             {/*<AddProduct/>*/}
         </div>
     );
--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -1,10 +1,49 @@
-// import React, {useState} from 'react';
-// import Input1 from "./UI/Input/Input1";
-// import NavButton from "./UI/Button/NavButton";
-//
-// const AddProduct = () => {
-//
-//
-// };
-//
-// export default AddProduct;
+import React, {useState} from 'react';
+import Input1 from "./UI/Input/Input1";
+import NavButton from "./UI/Button/NavButton";
+
+const AddProduct = ({create}) => {
+    const [title, setTitle] = useState('');
+    const [title1, setTitle1] = useState('');
+    const [title2, setTitle2] = useState('');
+
+    const addNewProduct = (e) => {
+        e.preventDefault();
+        const newProduct = {
+            id: Date.now(),
+            title,
+            title1,
+            title2
+        }
+        create(newProduct);
+        setTitle('')
+        setTitle1('')
+        setTitle2('')
+    };
+    return (
+<form>
+                <Input1
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type="text"
+                    placeholder="Название товара"
+                />
+                <Input1
+                    value={title1}
+                    onChange={e => setTitle1(e.target.value)}
+                    type="number"
+                    placeholder="Цена"
+                />
+                <Input1
+                    value={title2}
+                    onChange={e => setTitle2(e.target.value)}
+                    type="text"
+                    placeholder="Рейтинг"
+                />
+                <NavButton onClick={addNewProduct}>Отправить</NavButton>
+</form>
+
+)
+};
+
+export default AddProduct;
